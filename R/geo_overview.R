f6f3bd45d3ad095792a856de1df1ede04fdbc52e
wenzmo <wenzler@izw-berlin.de>
2021-07-08 21:01:10 +0200

    add geo_overview function
    shows the folders with geodata

---
+++ R/geo_overview.R
@@ -0,0 +1,30 @@
+#' show overview of geodata.
+#' @param path the path to the dropbox folder
+#' @return a list with folder names
+#' @export
+#' \dontrun{
+#' geo_overview()
+#' }
+
+geo_overview <- function(path = getwd(), folder){
+  # show main folders
+
+  main_fold <- list.dirs(here::here(path, "data-raw"), recursive = FALSE, full.names = FALSE)
+  if(folder %in% "main"){
+  print(main_fold)
+  }
+  # show sub folders
+  if(folder %in% "sub"){
+  sub_fold <- lapply(main_fold,
+                     function(x){list.dirs(here::here(path, "data-raw", x),
+                                           recursive = FALSE,
+                                           full.names = FALSE)})
+  names(sub_fold) <- main_fold
+
+  print(sub_fold)
+  }
+
+
+}
+
+#geo_overview(folder = "sub")
