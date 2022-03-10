4022ed1ff00a9e6c59cce57c3bbabf5b566bf908
wenzmo <wenzler@izw-berlin.de>
2022-03-10 12:26:49 +0100

    added a path function to get the path
    After searching for the foldwers you can decide, if you want the path of the one of the sub folders

--- R/geo_overview.R
+++ R/geo_overview.R
@@ -7,17 +7,16 @@
 #' geo_overview()
 #' }
 
-geo_overview <- function(path = getwd(), folder = c("sub","main")[utils::menu(c("sub","main"), title = "choose folder:")]){
+geo_overview <- function(folder = c("main", "sub")[utils::menu(c("main", "sub"), title = "choose folder:")]){
   # show main folders
-
-  main_fold <- list.dirs(here::here(path, "data-raw"), recursive = FALSE, full.names = FALSE)
+  main_fold <- list.dirs(here::here(base::getwd(), "GeoData", "data-raw"), recursive = FALSE, full.names = FALSE)
   if(folder %in% "main"){
   print(main_fold)
   }
   # show sub folders
   if(folder %in% "sub"){
   sub_fold <- lapply(main_fold,
-                     function(x){list.dirs(here::here(path, "data-raw", x),
+                     function(x){list.dirs(here::here(base::getwd(), "GeoData", "data-raw", x),
                                            recursive = FALSE,
                                            full.names = FALSE)})
   names(sub_fold) <- main_fold
@@ -25,8 +24,23 @@ geo_overview <- function(path = getwd(), folder = c("sub","main")[utils::menu(c(
   print(sub_fold)
   }
 
+  path <- c("yes", "no")[utils::menu(c("yes", "no"), title = "Do you need the path?")]
+  if(path %in% "yes"){
+    main_folder <- list.dirs(here::here(base::getwd(), "GeoData", "data-raw"), recursive = FALSE, full.names = FALSE)[
+      utils::menu(list.dirs(here::here(base::getwd(), "GeoData", "data-raw"), recursive = FALSE, full.names = FALSE),
+                  title = "which main folder?")
+    ]
+    sub_folder <- list.dirs(here::here(base::getwd(), "GeoData", "data-raw", main_folder), recursive = FALSE, full.names = FALSE)[
+      utils::menu(list.dirs(here::here(base::getwd(), "GeoData", "data-raw", main_folder), recursive = FALSE, full.names = FALSE),
+                  title = "which data file?")
+    ]
 
+  final_path <- here::here(base::getwd(), "GeoData", "data-raw", main_folder, sub_folder)
+  }
+  return(final_path)
 }
+
+
 #devtools::install_local(force = TRUE)
 # d6geodata::geo_overview(path = "C:/Users/wenzler/PopDynIZW Dropbox/GeoData/",folder = "main")
 
