b27c13f17168ad5eb1784a77056db3ef1b11214d
wenzmo <wenzler@izw-berlin.de>
2022-03-28 09:31:23 +0200

    fixed false namespace

--- R/build_meta_file.R
+++ R/build_meta_file.R
@@ -45,7 +45,7 @@ build_meta_file <- function(path = "."){
     doit <- c("Yes", "No")[utils::menu(c("Yes", "No"), title = "Do you want to change something?")]
   }
 
-  xlsx::write.csv(data, base::paste0(path, "/meta_data_", data$name, ".csv"),
+  utilswrite.csv(data, base::paste0(path, "/meta_data_", data$name, ".csv"),
                    row.names = FALSE)
   return(data)
 }
