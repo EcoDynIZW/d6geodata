28e2ca1293ede9a9daf32718db068ab5d285f277
wenzmo <wenzler@izw-berlin.de>
2022-04-01 11:29:42 +0200

    splitted redner geodata into raster an shapefile functions

---
+++ R/render_geodata_ras.R
@@ -0,0 +1,16 @@
+#' function for rendering an markdown to get an html of available geodata
+#' @param path The path where the data has to be stored
+#' @return a tibble where you have to set the parameters by hand.
+#' @export
+#' @examples
+#' \dontrun{
+#' render_geodata()
+#' }
+
+render_geodata_ras <-
+  function(path) {
+    rmarkdown::render(here::here("R", "geodata_template_raster_html.Rmd"),
+                      output_format = "html_document",
+                      output_dir = path,
+                      output_file = unlist(stringi::stri_split(path, regex = "/"))[length(unlist(stringi::stri_split(path, regex = "/")))])
+  }
