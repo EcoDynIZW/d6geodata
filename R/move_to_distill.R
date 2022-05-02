494ecc281deed75dc423dd4b304b2178cc17bdf0
wenzmo <wenzler@izw-berlin.de>
2022-05-02 21:36:07 +0200

    fixed some errors

--- R/move_to_distill.R
+++ R/move_to_distill.R
@@ -2,16 +2,16 @@
 #' @param path The path where the data lays
 #' @export
 #' @examples
-#' \donotrun{
+#' \dontrun{
 #' move_to_distill()
 #' }
 
 move_to_distill <-
-  function(path, data_name) {
-    file.copy(from = here::here("R", "geodata_template_html.Rmd"), to = "C:\Users\wenzler\Documents\GitHub\d6geodatabase")
-    file.rename(from = file.path("C:\Users\wenzler\Documents\GitHub\d6geodatabase",
+  function(path) {
+    base::file.copy(from = here::here("R", "geodata_template_html.Rmd"), to = "C:/Users/wenzler/Documents/GitHub/d6geodatabase")
+    base::file.rename(from = base::file.path("C:/Users/wenzler/Documents/GitHub/d6geodatabase",
                                  "geodata_template_html.Rmd"),
-                to = file.path("C:\Users\wenzler\Documents\GitHub\d6geodatabase",
-                               unlist(stringi::stri_split(path, regex = "/"))[length(unlist(stringi::stri_split(path, regex = "/")))]))
+                to = base::file.path("C:/Users/wenzler/Documents/GitHub/d6geodatabase",
+                                     base::unlist(stringi::stri_split(path, regex = "/"))[length(base::unlist(stringi::stri_split(path, regex = "/")))]))
                      # output_file = unlist(stringi::stri_split(path, regex = "/"))[length(unlist(stringi::stri_split(path, regex = "/")))])
   }
