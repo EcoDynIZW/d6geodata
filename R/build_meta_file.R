38e6fe5bce745cad0b0cbc5ecbacf2d4d6de0c5e
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2022-05-09 17:22:35 +0200

    changed write.csv to write.table

--- R/build_meta_file.R
+++ R/build_meta_file.R
@@ -47,8 +47,8 @@ build_meta_file <- function(path = "."){
     doit <- c("Yes", "No")[utils::menu(c("Yes", "No"), title = "Do you want to change something?")]
   }
 
-  utils::write.csv(data, base::paste0(path, "/meta_data_", data$folder_name, ".csv"),
-                   row.names = FALSE)
+  utils::write.table(data, base::paste0(path, "/meta_data_", data$folder_name, ".csv"),
+                   row.names = FALSE, sep = ",")
   return(data)
 }
 
