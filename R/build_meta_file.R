1b06d7943db865faaee248d8f3f3edfbc54d5dd4
wenzmo <wenzler@izw-berlin.de>
2022-05-13 14:26:21 +0200

    changed naming of arguments

--- R/build_meta_file.R
+++ R/build_meta_file.R
@@ -8,7 +8,7 @@
 #' }
 
 
-build_meta_file <- function(path = ".", folder_name){
+build_meta_file <- function(path = ".", data_name = ""){
 
   data <- dplyr::tibble(folder_name = NA,
                         name = NA,
@@ -31,8 +31,8 @@ build_meta_file <- function(path = ".", folder_name){
 
     data <- data %>%
       dplyr::mutate(
-        folder_name = folder_name,
-        name = stringi::stri_replace_last_fixed(data$folder_name, "_", "."),
+        folder_name = data_name,
+        name = stringi::stri_replace_last_fixed(data$data_name, "_", "."),
         epsg = ifelse(is.na(data$epsg), fun_epsg(), data$epsg),
         crs = suppressWarnings(sf::st_crs(data$epsg)$proj4string),
         year_of_data = ifelse(is.na(data$year_of_data), fun_num("year of data"), data$year_of_data),
