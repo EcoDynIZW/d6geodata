4a72854ba968ccbe2e35a08a30770492f61df551
wenzmo <wenzler@izw-berlin.de>
2022-05-17 10:44:51 +0200

    path to dropbox
    for different dropbox locations

---
+++ R/get_dropbox_path.R
@@ -0,0 +1,18 @@
+#' get the path to our dropbox
+#' @return the path to the dropbox
+#' @export
+#' @examples
+#' \dontrun{
+#' get_dropbox_path()
+#' }
+
+get_dropbox_path <- function(){
+  if(dir.exists("PopDynIZW Dropbox")){
+    paste(stringi::stri_c((unlist(
+      stringi::stri_split(here::here(), regex = '/')
+    )[1:3]), collapse = '/'), 'PopDynIZW Dropbox', sep = "/")
+  } else{
+    "D:/PopDynIZW Dropbox"
+  }
+}
+
