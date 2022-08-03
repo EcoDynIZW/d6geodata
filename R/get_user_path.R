bbaaeaa991a8d3463c7402f85b6d965c3d5efad1
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2022-08-03 10:49:10 +0200

    updated get_user_path

--- R/get_user_path.R
+++ R/get_user_path.R
@@ -7,15 +7,8 @@
 #' }
 
 get_user_path <- function(){
-  if(!"PopDynIZW Dropbox" %in% unlist(
-    stringi::stri_split(here::here(), regex = '/'))){
-    stringi::stri_c((unlist(
-      stringi::stri_split(here::here(), regex = '/')
-    )[1:3]), collapse = '/')
-  } else{
-    gsub("D:/", "C:/",stringi::stri_c((unlist(
-      stringi::stri_split(here::here(), regex = '/')
-    )[1:3]), collapse = '/'))
-  }
+  stringi::stri_c((unlist(
+    stringi::stri_split(.libPaths()[1], regex = '/')
+  )[1:3]), collapse = '/')
 }
 
