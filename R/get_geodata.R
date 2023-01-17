9b1cc64d1a87e10fd5e09c038e7cbbe589570e65
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2023-01-17 11:56:22 +0100

    changed get geoadta to our new settings with cloud

--- R/get_geodata.R
+++ R/get_geodata.R
@@ -2,28 +2,47 @@
 #' @return the file in a folder named after the file
 #' @param data_name the name of the data folder (e.g. copied from the website)
 #' @param download_data do you want to download the data? Choose "TRUE" or "FALSE"
+#' @param path_to_cloud set the path where your cloud is located. Like "E:/PopDynCloud"
 #' @export
 #' @examples
 #' \dontrun{
 #' get_geodata()
 #' }
 
-get_geodata <- function(data_name, download_data = FALSE){
-  meta <- utils::read.csv(here::here("data-proc", "meta_data_overview_proc.csv"))
+get_geodata <-
+  function(data_name, path_to_cloud, download_data = FALSE) {
+    tif <-
+      terra::rast(
+        paste(
+          path_to_cloud,
+          "Geodata",
+          "data-proc",
+          stringi::stri_c(unlist(
+            stringi::stri_split(data_name, regex = "_")
+          )[2]),
+          data_name,
+          stringi::stri_replace_last_fixed(data_name, "_", "."),
+          sep = "/"
+        )
+      )
 
-  tif <- raster::raster((meta %>% dplyr::filter('folder_name' %in% data_name))$dropbox_link)
+    if (download_data %in% TRUE) {
+      dir.create(here::here("data-raw", data_name))
 
-     if(download_data %in% TRUE){
+base::file.copy(from = paste(
+  path_to_cloud,
+  "Geodata",
+  "data-proc",
+  stringi::stri_c(unlist(stringi::stri_split(data_name, regex = "_"))[2]),
+  data_name,
+  sep = "/"
+),
+to = here::here("data-raw"),
+recursive = TRUE)
 
-       base::dir.create(path = here::here("data-raw", data_name))
+  }
 
-       raster::writeRaster(tif, here::here(base::gsub("_tif", ".tif", data_name)))
-
-
-
-     }
-
-  return(tif)
-}
+    return(tif)
+    }
 
 
