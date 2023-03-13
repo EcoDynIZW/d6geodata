1159d040136d94ff7a95fb87e3eca98e3c58d70f
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2023-03-13 09:50:50 +0100

    changed legend output. other always grey

--- R/plot_qualitative_map.R
+++ R/plot_qualitative_map.R
@@ -8,39 +8,30 @@
 #' }
 
 plot_qualitative_map <- function(tif){
-
-  if(length(unique(terra::values(tif))) >= 5){
-    tif_df <-  dplyr::tibble(cat = tif %>% terra::values() %>% as.vector()) %>%
-      dplyr::count(cat, sort = TRUE) %>%
-      dplyr::mutate(cat_fct = ifelse(dplyr::row_number() > 5, 0, cat))
-
+  if (length(unique(terra::values(tif))) >= 5) {
+    tif_df <- dplyr::tibble(cat = tif %>% terra::values() %>%
+                              as.vector()) %>% dplyr::count(cat, sort = TRUE) %>%
+      dplyr::mutate(cat_fct = ifelse(dplyr::row_number() >
+                                       5, 0, cat))
     tif_legend <- tif %>% terra::classify(rcl = matrix(c(tif_df$cat[-c(1:5)],
-                                                  rep(0, nrow(tif_df)-5)), ncol = 2, nrow = nrow(tif_df)-5))
-
+                                                         rep(0, nrow(tif_df) - 5)), ncol = 2, nrow = nrow(tif_df) -
+                                                         5))
     tif_plot <- tif_legend
-
     terra::values(tif_plot) <- terra::as.factor(terra::values(tif_plot))
-
-    ggplot2::ggplot() +
-      stars::geom_stars(data = stars::st_as_stars(tif_plot)) +
-      ggplot2::coord_sf(expand = FALSE) +
-      rcartocolor::scale_fill_carto_d(
+    ggplot2::ggplot() + stars::geom_stars(data = stars::st_as_stars(tif_plot)) +
+      ggplot2::coord_sf(expand = FALSE) + rcartocolor::scale_fill_carto_d(
         palette = "Bold",
         name = NULL,
-        labels = c("other", terra::sort(unique(terra::values(tif_legend)))[-1])
-      ) +
+        labels = forcats::fct_reorder(.f = c(terra::sort(unique(terra::values(tif_legend)))[-1], "other"),
+                                      .x = c(terra::sort(unique(terra::values(tif_legend)))[-1], "other")
+        )) +
       ggplot2::guides(fill = ggplot2::guide_legend(label.position = "bottom"))
-  } else{
+  }
+  else {
     tif_plot <- tif
     terra::values(tif_plot) <- terra::as.factor(terra::values(tif_plot))
-    return(
-      ggplot2::ggplot() +
-        stars::geom_stars(data = stars::st_as_stars(tif_plot)) +
-        ggplot2::coord_sf(expand = FALSE) +
-        rcartocolor::scale_fill_carto_d(
-          palette = "Bold",
-          name = NULL
-        ) +
-        ggplot2::guides(fill = ggplot2::guide_legend(label.position = "bottom")))
+    return(ggplot2::ggplot() + stars::geom_stars(data = stars::st_as_stars(tif_plot)) +
+             ggplot2::coord_sf(expand = FALSE) + rcartocolor::scale_fill_carto_d(palette = "Bold",
+                                                                                 name = NULL) + ggplot2::guides(fill = ggplot2::guide_legend(label.position = "bottom")))
   }
 }
