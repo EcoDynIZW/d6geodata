8c5b2802f99cf0ae679e8adea41d07d036f13610
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2023-06-30 12:47:02 +0200

    updated description

--- R/temp_raster_berlin.R
+++ R/temp_raster_berlin.R
@@ -1,13 +1,17 @@
-#' get template raster for Berlin
+#' Get template raster for Berlin. This can be used to rasterize vecotr data within Berlin borders.
+#' This is made by using the copernicus database rasters as template.
+#'
 #' @return a template spatRaster
-#' @param resolution resolution in meters
+#'
+#' @param resolution resolution in meters, default 10 meters
+#'
 #' @export
 #' @examples
 #' \dontrun{
-#' temp_rast(resolution)
+#' temp_rast(resolution = 10)
 #' }
 
-temp_ras <- function(resolution){
+temp_ras <- function(resolution = 10){
   terra::rast(
     crs = sf::st_crs(3035)$proj4string,
     extent = c(4531040, 4576740, 3253790, 3290790),
