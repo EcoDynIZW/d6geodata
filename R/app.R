d7133b89dde02b4ce5a94694be6eea2788e2d5fa
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2024-09-23 09:58:07 +0200

    added shiny app for accesing data

---
+++ R/app.R
@@ -0,0 +1,145 @@
+
+library(shiny)
+library(patchwork)
+library(shinyWidgets)
+library(terra)
+library(sf)
+library(shinyFiles)
+
+getgeodataApp <- function(){
+
+geopath <- base::readline("path to dep6:")
+
+main_folders <- lapply(
+  X = list.dirs(
+    path = paste(geopath, "GeoData", "data", sep = "/"), 
+    recursive = F,
+    full.names = F
+    ),
+  FUN = function(x){
+    gsub("\\./", "", x)
+            }
+  )
+
+sub_folders <- lapply(main_folders, function(x) {
+  subs <- gsub("\\./",
+               "",
+               list.dirs(
+                 path = file.path(paste(geopath, "GeoData", "data", sep = "/"), x), 
+                 recursive = FALSE,
+                 full.names = FALSE
+               ))
+  
+  # Filter out unwanted subfolders (e.g., _archive)
+  subfolders <- subs[!subs %in% c("_archive")]
+  return(subfolders)
+}
+  )
+
+names(sub_folders) <- main_folders
+
+ui <- fluidPage(
+  titlePanel("Show And Download Geodata"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("mainfolder", "Select Mainfolder:", 
+                  choices = c(None='.', main_folders), width = "100%"),
+      uiOutput("subfolderUI"),
+    
+      shinyDirButton("directory", "Choose Directory and Download", "Please select a directory")
+    ),
+    
+    mainPanel(
+      uiOutput("iframeOutput")      # Dynamic iframe
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  # Update subfolder choices based on selected mainfolder
+  observeEvent(input$mainfolder, {
+    subfolders <- sub_folders[[input$mainfolder]] # Access subfolder list by mainfolder name
+    
+    updateSelectInput(session, "subfolder", 
+                      choices = c(None='.', subfolders))
+    
+  })
+  
+  # Render the subfolder selectInput dynamically based on the selected mainfolder
+  output$subfolderUI <- renderUI({
+    selectInput("subfolder", "Select Subfolder:", 
+                choices = NULL) # Initially empty; will be updated based on mainfolder
+  })
+  
+  shinyDirChoose(input, "directory", roots = c(home = getwd()), session = session)
+  
+  selected_dir <- reactive({
+    req(input$directory)
+    parseDirPath(c(home = getwd()), input$directory)
+    
+    
+  })
+  
+  full_path <- reactive({
+    req(input$mainfolder,input$subfolder)
+    file.path(paste(geopath, "GeoData", "data", sep = "/"), paste(input$mainfolder,input$subfolder, # selected_dir()
+                                      sep = "/"))
+  })
+  
+  observeEvent(input$directory, {
+    req(full_path(), selected_dir())
+    
+    # Create the target directory if it doesn't exist
+    base::dir.create(path = selected_dir(), showWarnings = TRUE, recursive = TRUE)
+    
+    # Get the size of the file for progress tracking (if it's a single file)
+    file_size <- sum(sapply(list.files(full_path(), full.names = TRUE, recursive = FALSE), file.size))
+    
+    files_to_copy <- list.files(full_path(), full.names = TRUE, recursive = FALSE, include.dirs = FALSE)
+    
+    # Show a progress bar that updates based on real copy time
+    withProgress(message = 'Copying files...', value = 0, {
+      
+      num_files <- length(files_to_copy)
+      if (num_files == 0) {
+        showNotification("No files to copy!", type = "warning")
+        return(NULL)
+      }
+      
+      # Copy each file and update the progress
+      for (i in seq_along(files_to_copy)) {
+        file.copy(from = files_to_copy[i], to = selected_dir(), overwrite = TRUE)
+        
+        # Update the progress bar
+        incProgress(1 / num_files, detail = paste("Copying file", i, "of", num_files))
+      }
+      
+      # Notify user that the folder has been copied
+      showNotification(paste("Folder copied to:", selected_dir()), type = "message")
+    })  
+  })
+
+  # Render the iframe dynamically based on the selected mainfolder and subfolder
+  output$iframeOutput <- renderUI({
+    if (!is.null(input$subfolder)) {
+      url <- paste0("https://ecodynizw.github.io/posts_geodata/", input$subfolder)
+      
+      tags$iframe(
+        src = url,
+        style = "width:100%;height:600px;border:none;"
+      )
+    } else {
+      tags$p("No subfolder selected.")
+    }
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+}
+
+
+
